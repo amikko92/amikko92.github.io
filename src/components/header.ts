341c60de3408e864e4e5828f3d003b6639fd03b6
amikko92 <a.mikko92@gmail.com>
2023-08-04 22:26:12 +0200

    Navigation links

--- src/components/header.ts
+++ src/components/header.ts
@@ -2,29 +2,61 @@ const template = document.createElement("template");
 template.innerHTML = /*html*/ `
 <style>
 header {
+    display: flex;
     background-color: var(--primary-color);
-    padding: 1em;
+    align-items: center;
+}
+
+nav {
+    margin: 1em;
 }
 
 a {
     text-decoration: none;
+    font-family: "Chakra Petch", sans-serif;
+    font-size: 1.5rem;
+    margin: 1em;
+}
+
+a:link {
+    color: white;
+}
+
+a:visited {
+    color: white;
+}
+
+a:hover {
+    color: white;
+    background: var(--primary-dark-color);
+}
+
+a:active {
+    color: white;
+    background: var(--primary-light-color);
 }
 
 .logo {
     font-family: "Press Start 2P", cursive;
-    font-size: 1.2rem;
-    text-align: left;
+    font-size: 1rem;
+    text-align: center;
     color: white;
     text-shadow: 2px 2px var(--primary-dark-color);
+    margin: 1em;
 }
 </style>
 
 <header>
-    <a href="../">
-        <div class="logo">
-            Andreas Mikko
-        </div>
+    <a class="logo" href="/">
+        <div>Andreas</div>
+        <div>Mikko</div>
     </a>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/projects/">Projects</a>
+        <a href="/about/">About</a>
+        <a href="/contact/">Contact</a>
+    </nav>
 </header>
 `;
 
